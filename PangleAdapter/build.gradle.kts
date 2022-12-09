eeace4c509e554238b5bc0f62ad50747aea3b27d
Jose Padilla <jose.padilla@chartboost.com>
2022-12-09 12:07:56 -0800

    [HB-4895]: Update contact email. (#13)

--- PangleAdapter/build.gradle.kts
+++ PangleAdapter/build.gradle.kts
@@ -128,7 +128,7 @@ afterEvaluate {
                         developer {
                             id.set("chartboostmobile")
                             name.set("chartboost mobile")
-                            email.set("mobile@chartboost.com")
+                            email.set("support@chartboost.com")
                         }
                     }
 
