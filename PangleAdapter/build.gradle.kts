4421917304830cfc9404335ced24a9cf7fdcc393
Bichen Wang <bichenwang@gmail.com>
2023-03-23 16:10:49 -0700

    .1 updates for CM sdk 4.0.0 hard peg

--- PangleAdapter/build.gradle.kts
+++ PangleAdapter/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.4.9.1.3.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.4.9.1.3.1"
         buildConfigField("String", "CHARTBOOST_MEDIATION_PANGLE_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -68,7 +68,7 @@ dependencies {
 
     // For external usage, please use the following production dependency.
     // You may choose a different release version.
-    "remoteImplementation"("com.chartboost:chartboost-mediation-sdk:4.+")
+    "remoteImplementation"("com.chartboost:chartboost-mediation-sdk:4.0.0")
 
     // Partner SDK
     implementation("com.pangle.global:ads-sdk:4.9.1.3")
